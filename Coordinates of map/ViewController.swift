88332aab39f5ab781595567cbd332ee6c4f80711
Justin Chung <justinchung2026@cdnis.edu.hk>
2024-03-21 12:48:08 +0800

    Created long, lat, pinpoint and map function In this commit it allowed me to use a pinpoint to show the location of where a person have choose on the map. While they click and hold the pinpoint will show up and also show a textbox below of the longitude and latitude of the location they have clicked.

--- Coordinates of map/ViewController.swift
+++ Coordinates of map/ViewController.swift
@@ -6,12 +6,45 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIGestureRecognizerDelegate {
+    @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var cordLabel: UILabel!
+    var latitude = Float()
+    var longitude = Float()
+    var hasAnnotation = false
+    let pin = MKPointAnnotation()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+   
+        mapView = MKMapView(frame: view.bounds)
+        view.addSubview(mapView)
+        let longPressGesture = UILongPressGestureRecognizer(target: self, action: #selector(handleLongPress(_:))) // when view loads, check if gesture is made, then if gesture made run function "HandleLongPress'
+        mapView.addGestureRecognizer(longPressGesture) // connects the gesture recogniser to the map
+        self.view.bringSubviewToFront(cordLabel)
+    }
+    
+    @objc func handleLongPress(_ gestureRecognizer: UILongPressGestureRecognizer)
+    {
+        if gestureRecognizer.state == .began
+        {
+            if hasAnnotation == true // if there is alr a pin on the map
+            {
+                mapView.removeAnnotation(pin)
+            }
+            let touchPoint = gestureRecognizer.location(in: mapView)
+            let coordinate = mapView.convert(touchPoint, toCoordinateFrom: mapView)
+            // adds a pin to show where the location is
+            pin.coordinate = coordinate // putting the pin location to coordinate
+            mapView.addAnnotation(pin)
+            // to get the latitude and longtitude from the map
+            latitude = Float(coordinate.latitude)
+            longitude = Float(coordinate.longitude)
+            cordLabel.text = "Lat: \(latitude), Long: \(longitude)"
+            hasAnnotation = true
+        }
     }
 
 
